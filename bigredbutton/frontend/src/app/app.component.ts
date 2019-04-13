7dab8a0dd84b659a906ecf7df79d92d29f670437
Daniel <d.solodov@wis.software>
2019-04-13 16:15:37 +0300

    Update angular version, add preloader and animations

--- bigredbutton/frontend/src/app/app.component.ts
+++ bigredbutton/frontend/src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<app-call-btn></app-call-btn>`,
-  styles: []
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
